3a976a7b5d4ed8dd0bc39fdb6f757c2bd9b223bf
Antoine BERNIER <antoine.bernier@gmail.com>
2024-07-05 13:33:41 +0200

    ^all

--- apps/react-spring-animations/src/index.jsx
+++ apps/react-spring-animations/src/index.jsx
@@ -13,9 +13,9 @@ function App() {
   // We use this spring to interpolate all the colors, position and rotations
   const [{ x }] = useSpring({ x: toggle, config: { mass: 5, tension: 1000, friction: 50, precision: 0.0001 } }, [toggle])
   return (
-    <a.div class="container" style={{ backgroundColor: x.to([0, 1], ["#c9ffed", "#ff2558"]), color: x.to([0, 1], ["#7fffd4", "#c70f46"]) }}>
-      <h1 class="open" children="<h1>" />
-      <h1 class="close" children="</h1>" />
+    <a.div className="container" style={{ backgroundColor: x.to([0, 1], ["#c9ffed", "#ff2558"]), color: x.to([0, 1], ["#7fffd4", "#c70f46"]) }}>
+      <h1 className="open" children="<h1>" />
+      <h1 className="close" children="</h1>" />
       <a.h1>{x.to((x) => (x + 8).toFixed(2))}</a.h1>
       <Scene x={x} set={set} />
     </a.div>
