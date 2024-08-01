eb6bf119a3d6b6c6610e2ae75c647789c1656217
rndexe <himanshu.ernde@gmail.com>
2024-08-01 15:08:08 +0530

    Fix arkanoid demo

--- demos/arkanoid/src/index.jsx
+++ demos/arkanoid/src/index.jsx
@@ -1,6 +1,5 @@
-import { render } from "react-dom"
-import React from "react"
+import { createRoot } from "react-dom/client"
 import App from "./App"
 import "./styles.css"
 
-render(<App />, document.getElementById("root"))
+createRoot(document.getElementById("root")).render(<App />)
