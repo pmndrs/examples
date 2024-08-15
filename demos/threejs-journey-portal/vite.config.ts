f225f028013e3669ae1ba828c70e94df81dd9b87
rndexe <himanshu.ernde@gmail.com>
2024-08-15 19:09:23 +0530

    Fix threejs portal demo

--- demos/threejs-journey-portal/vite.config.ts
+++ demos/threejs-journey-portal/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import glslify from 'vite-plugin-glslify/dist/index.mjs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), glslify({ include: [/\.jsx$/] })],
 })
