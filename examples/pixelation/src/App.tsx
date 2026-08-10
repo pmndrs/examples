d6987f43727f8caf046de284362d6791d288ce02
kvvasuu <lukaszkwas96@gmail.com>
2026-08-10 22:25:08 +0200

    Add Pixelation example, ported from react-postprocessing's CodeSandbox demo

---
+++ examples/pixelation/src/App.tsx
@@ -0,0 +1,25 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Stage } from "@react-three/drei";
+import { EffectComposer, Pixelation } from "@react-three/postprocessing";
+import { useControls } from "leva";
+import Model from "./Model";
+
+export default function App() {
+  const { granularity } = useControls({
+    granularity: { value: 5, min: 1, max: 30, step: 1 },
+  });
+
+  return (
+    <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+      <Suspense fallback={null}>
+        <EffectComposer>
+          <Pixelation granularity={granularity} />
+        </EffectComposer>
+        <Stage preset="rembrandt" intensity={1} environment="city">
+          <Model />
+        </Stage>
+      </Suspense>
+    </Canvas>
+  );
+}
